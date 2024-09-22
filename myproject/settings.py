a870c70fd11be9a7e5358e17878131934de24c65
Mulusew Wube <158047600+mulusewwube@users.noreply.github.com>
2024-09-22 14:16:57 +0300

    Update settings.py
    db1

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,20 +92,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-
-# # Database
-# # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'cfPYVIwFqNPUKzPiCnqInhHLgDIkkLCU',
-#         'HOST': 'postgres.railway.internal',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config(default="postgresql://postgres:UtcNpCogIMPuQxvChXZWRlmqMeqhLFdC@postgres.railway.internal:5432/railway")
+}
 
 
 
